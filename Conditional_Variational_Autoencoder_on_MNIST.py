c9088f771fe9893374bcc95766b9d0e781da857f
Stijn Lafontaine <s.c.lafontaine@student.tudelft.nl>
2022-03-22 13:58:55 +0100

    actual files of classes CVAE

---
+++ Conditional_Variational_Autoencoder_on_MNIST.py
@@ -0,0 +1,170 @@
+from matplotlib import pyplot as plt
+from matplotlib.offsetbox import OffsetImage, AnnotationBbox, TextArea
+import matplotlib
+import numpy as np
+import pandas as pd
+import torch
+import torchvision
+from torch import nn
+from torch.autograd import Variable
+from torch.utils.data import DataLoader
+from torch.utils import data
+import torch.nn.functional as F
+from torchvision import transforms
+from torchvision.datasets import MNIST
+from torchvision.utils import save_image
+from sklearn.model_selection import train_test_split
+from tqdm import tqdm, tqdm_notebook
+from sklearn.manifold import TSNE
+from sklearn.decomposition import PCA
+
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+print('Training on',DEVICE)
+SEED = 42
+np.random.seed(SEED)
+torch.manual_seed(SEED)
+torch.cuda.manual_seed(SEED)
+torch.backends.cudnn.deterministic = True
+
+batch_size = 128
+learning_rate = 0.005
+input_size = 28*28
+hidden_size = 12
+labels_length = 10
+
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+    transforms.Normalize((0.5,), (0.5,))
+])
+
+dataset = MNIST('./data', transform=transform, download=True)
+train_data, test_data = data.random_split(dataset, (50000,10000))
+
+train_dataset = DataLoader(train_data, batch_size=batch_size, shuffle=True)
+val_dataset = DataLoader(test_data, batch_size=batch_size, shuffle=True)
+
+#helper functions
+def one_hot(x, max_x):
+    return torch.eye(max_x + 1)[x]
+
+def plot_gallery(images, h, w, n_row=3, n_col=6):
+    plt.figure(figsize=(2 * n_col, 2 * n_row))
+    for i in range(n_row * n_col):
+        plt.subplot(n_row, n_col, i + 1)
+        plt.axis("off")
+        plt.imshow(images[i].reshape(h, w), cmap = matplotlib.cm.binary)
+    #plt.show()
+    
+def plot_loss(history):
+    loss, val_loss = zip(*history)
+    plt.figure(figsize=(15, 9))
+    plt.plot(loss, label="train_loss")
+    plt.plot(val_loss, label="val_loss")
+    plt.legend(loc='best')
+    plt.xlabel("epochs")
+    plt.ylabel("loss")
+    plt.show()
+
+
+class CVAE(nn.Module):
+    def __init__(self, input_size, hidden_size=20):
+        super(CVAE, self).__init__()
+        input_size_with_label = input_size + labels_length
+        hidden_size += labels_length
+        
+        self.fc1 = nn.Linear(input_size_with_label,512)
+        self.fc21 = nn.Linear(512, hidden_size)
+        self.fc22 = nn.Linear(512, hidden_size)
+        
+        self.relu = nn.ReLU()
+        
+        self.fc3 = nn.Linear(hidden_size, 512)
+        self.fc4 = nn.Linear(512, input_size)
+    
+    def encode(self, x, labels):
+        x = x.view(-1, 1*28*28)
+        x = torch.cat((x, labels), 1)
+        x = self.relu(self.fc1(x))
+        return self.fc21(x), self.fc22(x)
+        
+    def decode(self, z, labels):
+        torch.cat((z, labels), 1)
+        z = self.relu(self.fc3(z))
+        return torch.sigmoid(self.fc4(z))
+        
+    def reparameterize(self, mu, logvar):
+        std = torch.exp(0.5 *logvar)
+        eps = torch.randn_like(std)
+        return eps.mul(std).add_(mu)
+        
+    def forward(self,x, labels):
+        #targets = one_hot(targets,labels_length-1).float().to(DEVICE)
+        mu, logvar = self.encode(x, labels)
+        z = self.reparameterize(mu, logvar)
+        x = self.decode(z, labels)
+        return x, mu, logvar
+
+def train_cvae(net, dataloader, test_dataloader, flatten=True, epochs=20):
+    validation_losses = []
+    optim = torch.optim.Adam(net.parameters())
+
+    log_template = "\nEpoch {ep:03d} val_loss {v_loss:0.4f}"
+    with tqdm(desc="epoch", total=epochs) as pbar_outer:  
+        for i in range(epochs):
+            for batch, labels in dataloader:
+                batch = batch.to(DEVICE)
+                labels = one_hot(labels,9).to(DEVICE)
+
+                if flatten:
+                    batch = batch.view(batch.size(0), 28*28)
+
+                optim.zero_grad()
+                x,mu,logvar = net(batch, labels)
+                loss = vae_loss_fn(batch, x[:, :784], mu, logvar)
+                loss.backward()
+                optim.step()
+            evaluate(validation_losses, net, test_dataloader, flatten=True)
+            pbar_outer.update(1)
+            tqdm.write(log_template.format(ep=i+1, v_loss=validation_losses[i]))
+    return validation_losses
+
+cvae = CVAE(28*28).to(DEVICE)
+
+def vae_loss_fn(x, recon_x, mu, logvar):
+    BCE = F.binary_cross_entropy(recon_x, x, reduction='sum')
+    KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+    return BCE + KLD
+
+def evaluate(losses, autoencoder, dataloader, flatten=True):
+    model = lambda x, y: autoencoder(x, y)[0]    
+    loss_sum = []
+    inp, out = [],[]
+    loss_fn = nn.MSELoss()
+    for inputs, labels in dataloader:
+        inputs = inputs.to(DEVICE)
+        labels = one_hot(labels,9).to(DEVICE)
+
+        if flatten:
+            inputs = inputs.view(inputs.size(0), 28*28)
+
+        outputs = model(inputs, labels)
+        loss = loss_fn(inputs, outputs)            
+        loss_sum.append(loss)
+        inp = inputs
+        out = outputs
+
+    with torch.set_grad_enabled(False):
+        plot_gallery([inp[0].detach().cpu(),out[0].detach().cpu()],28,28,1,2)    
+
+    losses.append((sum(loss_sum)/len(loss_sum)).item())
+
+history = train_cvae(cvae, train_dataset, val_dataset)
+
+val_loss = history
+plt.figure(figsize=(15, 9))
+plt.plot(val_loss, label="val_loss")
+plt.legend(loc='best')
+plt.xlabel("epochs")
+plt.ylabel("loss")
+plt.show()
